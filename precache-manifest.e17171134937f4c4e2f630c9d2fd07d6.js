b98dd5b944d98913be78db7773572026cc86a092
Csaba Králik <kralikcsabi@gmail.com>
2022-04-28 13:00:58 +0200

    Updates

---
+++ precache-manifest.e17171134937f4c4e2f630c9d2fd07d6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebf1c44e3de43bc7892cca56c0e72e8b",
+    "url": "/react-and-morty/index.html"
+  },
+  {
+    "revision": "a76fdf029e9a3e7f34e1",
+    "url": "/react-and-morty/static/css/main.97771aa6.chunk.css"
+  },
+  {
+    "revision": "b09921271abb026a610e",
+    "url": "/react-and-morty/static/js/2.4d5105ee.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-and-morty/static/js/2.4d5105ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a76fdf029e9a3e7f34e1",
+    "url": "/react-and-morty/static/js/main.5105a7fe.chunk.js"
+  },
+  {
+    "revision": "453b88591806e02178dd",
+    "url": "/react-and-morty/static/js/runtime-main.726c9020.js"
+  },
+  {
+    "revision": "4b4420cffe3ba4f13221e529e5b60078",
+    "url": "/react-and-morty/static/media/get_schwifty.4b4420cf.ttf"
+  },
+  {
+    "revision": "8881c2af39f45d57e154c218f962ed01",
+    "url": "/react-and-morty/static/media/image.8881c2af.png"
+  },
+  {
+    "revision": "e62030ffb1095f735481ad18c6404e52",
+    "url": "/react-and-morty/static/media/main.e62030ff.jpg"
+  }
+]);
